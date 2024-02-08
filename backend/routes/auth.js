fe900a755691027ae13a5e91fba7fecf8dd5eff2
Jiyun <grosjeanjiyun@icloud.com>
2024-02-08 21:59:28 +0100

    user routes

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,18 +1,27 @@
 import express from "express";
 import {
   forgotPassword,
+  getUserProfile,
   loginUser,
   logout,
   registerUser,
   resetPassword,
+  updatePassword,
+  updateProfile,
 } from "../controllers/authController.js";
+import { isAuthenticatedUser } from "../middlewares/auth.js";
 
 const router = express.Router(); // define router
 
 router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
 router.route("/logout").get(logout);
+
 router.route("/password/forgot").post(forgotPassword);
 router.route("/password/reset/:token").put(resetPassword);
+
+router.route("/me").get(isAuthenticatedUser, getUserProfile);
+router.route("/me/update").put(isAuthenticatedUser, updateProfile);
+router.route("/password/update").put(isAuthenticatedUser, updatePassword);
 
 export default router;
